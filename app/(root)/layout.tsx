6ebbb2928ce594cc7f1d5192da1025223b9342ab
Alecs Reasonda <momai.reason@gmail.com>
2024-02-10 13:58:38 -0800

    added speedinsights in layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import { ClerkProvider } from '@clerk/nextjs';
 import { Inter } from 'next/font/google';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Topbar from '@/components/shared/Topbar';
 import Leftbar from '@/components/shared/Leftbar';
 import Footer from '@/components/shared/Footer';
