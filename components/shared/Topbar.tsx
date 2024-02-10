cf8aebbba6d020b1ff9131c4aad8bbacc2f1078f
Alecs Reasonda <momai.reason@gmail.com>
2024-02-10 14:59:06 -0800

    fixed nav bar size on mobile and installed framer motion

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -18,7 +18,7 @@ const Topbar = () => {
     const smallNavLinkClass = "cursor-pointer mx-2 sm:mx-4 text-black-3 hover:translate-y-[-.5px] text-[1.2rem] hover:drop-shadow-sm block";
 
     return (
-        <nav className="top-bottom-bars w-[100%] justify-end px-4 py-2 border-b-2 border-black-3 shadow-1 fixed top-0 right-0 z-20">
+        <nav className="top-bottom-bars w-[100%] justify-end px-4 py-2 border-b-2 border-black-3 shadow-1 fixed top-0 right-0 z-20 min-h-[4rem] sm:h-auto">
             <Link href="/" className="uppercase mr-auto font-bold text-[.8rem] sm:text-[1rem] text-primary-3 hover:translate-y-[-.5px] hover:drop-shadow-sm">
                 Squad Gains
             </Link>
